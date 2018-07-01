6e8d2b252fa226832f48534f63c56e112651be54
Jesse Myers <jesse.myers@globality.com>
2018-07-01 12:19:59 -0700

    bump

--- logging_format/api.py
+++ logging_format/api.py
@@ -6,7 +6,7 @@ from logging_format.visitor import LoggingVisitor
 from logging_format.whitelist import Whitelist
 
 
-__version__ = "0.3.0"
+__version__ = "0.4.0"
 
 
 class LoggingFormatValidator(object):
