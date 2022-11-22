26053c968527c25eb970b530bcb58e7e1ff5eefc
sethisernhagen <seth.isernhagen@globality.com>
2022-11-22 15:36:14 -0500

    bumpversion to 0.9.0

--- logging_format/api.py
+++ logging_format/api.py
@@ -6,7 +6,7 @@ from logging_format.visitor import LoggingVisitor
 from logging_format.whitelist import Whitelist
 
 
-__version__ = "0.8.1"
+__version__ = "0.9.0"
 
 
 class LoggingFormatValidator(object):
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import find_packages, setup
 
 project = "flake8-logging-format"
-version = "0.8.1"
+version = "0.9.0"
 long_description = open("README.md").read()
 
 setup(
